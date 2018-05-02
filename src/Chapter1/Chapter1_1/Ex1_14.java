d6c555703fc2cb86e238d807352a3a8a96bbba6b
joymyhome <faith.zjing@gmail.com>
2018-05-02 00:53:12 -0700

    upload 1.1.14

---
+++ src/Chapter1/Chapter1_1/Ex1_14.java
@@ -0,0 +1,25 @@
+package Chapter1.Chapter1_1;
+
+import java.util.Scanner;
+
+public class Ex1_14 {
+
+	public static void main(String[] args) {
+		System.out.print("Please input an integer > 2:");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		int expo = 0;
+		int result = 1;
+		while(result < n/2+1)
+		{
+			result *= 2;
+			expo += 1;
+		}
+		System.out.println("The maximum integer smaller than log2N = "+ expo);
+		
+
+
+	}
+
+}
