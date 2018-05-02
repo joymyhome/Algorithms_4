061bad81320b371e2d9c802a01a5e41ab73a87d2
joymyhome <faith.zjing@gmail.com>
2018-05-01 17:24:07 -0700

    1.1.9

---
+++ Chapter1/Chapter1.1/Ex1_9.java
@@ -0,0 +1,22 @@
+package Chapter1.Chapter1_1;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_9 {
+
+	public static void main(String[] args) {
+		//将一个正整数N用二进制表示饼转换为一个String类型的值s
+		int input = StdIn.readInt();
+		//java 自带的实现方法
+		StdOut.println(Integer.toBinaryString(input));
+		//另一种解法：
+		String s = "";
+		for(int n = input; n >0; n/=2)
+			s = (n%2) + s;
+		StdOut.println(s);
+		
+
+	}
+
+}
