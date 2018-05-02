0073b39c0db71e08503c5fd6b38bc743585d15a8
joymyhome <faith.zjing@gmail.com>
2018-05-01 20:23:45 -0700

    1.1.11

---
+++ Chapter1/Chapter1.1/Ex1_11.java
@@ -0,0 +1,39 @@
+package Chapter1.Chapter1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_11 {
+
+	private static void printBoolean(boolean[][] bMatrix)
+	{
+		StdOut.print(" ");
+		
+		for (int i = 0; i<bMatrix.length; i++)
+			StdOut.print(i);
+		StdOut.print('\n');
+		
+		for (int i = 0; i<bMatrix.length; i++)
+		{
+			StdOut.print(i);
+			for(int j = 0; j<bMatrix[0].length; j++)
+			{
+				
+				StdOut.print((bMatrix[i][j] == true)?" ":"*");
+			}
+		StdOut.println();
+		}
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean[][] matrix = {{false,true,true},{true, false, true},{true, true, false}};
+		printBoolean(matrix);
+
+	}
+
+		
+
+		
+}
+	
+
