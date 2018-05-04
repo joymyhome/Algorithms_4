8fc19581c0e8f945a71d6f50a63a1c14cdc0af7f
joymyhome <faith.zjing@gmail.com>
2018-05-03 17:39:10 -0700

    1.2.4

---
+++ src/Chapter1/Chapter1_2/Ex2_4.java
@@ -0,0 +1,14 @@
+package Chapter1.Chapter1_2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex2_4 {
+	public static void main(String[] args) {
+		String string1 = "hello";
+		String string2 = string1;
+		string1 = "world";
+		StdOut.println(string1);
+		StdOut.println(string2);
+	}
+
+}
